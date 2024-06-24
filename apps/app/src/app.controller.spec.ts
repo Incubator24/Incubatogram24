a628ee39502c788a92af7c1e96eeca0bc1afbed1
Rosh95 <azi.rovshan@gmail.com>
2024-06-24 10:39:41 +0300

    fix gitignore

--- apps/app/src/app.controller.spec.ts
+++ apps/app/src/app.controller.spec.ts
@@ -1,21 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AppController } from './app.controller'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { ConfigType } from './config/configuration'
 
 describe('AppController', () => {
     let appController: AppController
+    let city
 
     beforeEach(async () => {
         const app: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
         }).compile()
 
+        const configService = app.get(ConfigService<ConfigType, true>)
+        city = configService.get('CITY')
         appController = app.get<AppController>(AppController)
     })
 
     describe('root', () => {
         it('should return "Hello World!"', () => {
-            expect(appController.getHello()).toBe('Hello World, Moscow')
+            console.log(city)
+            expect(appController.getHello()).toBe(`Hello World, ${city}`)
         })
     })
 })
--- apps/app/src/auth/auth.controller.spec.ts
+++ apps/app/src/auth/auth.controller.spec.ts
@@ -1,21 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthModule } from './auth.module'
 import { AuthController } from './auth.controller'
+import { ConfigService } from '@nestjs/config'
+import { ConfigType } from '../config/configuration'
 
 describe('AuthController', () => {
     let authController: AuthController
+    let city
 
     beforeEach(async () => {
         const app: TestingModule = await Test.createTestingModule({
             imports: [AuthModule],
         }).compile()
 
+        const configService = app.get(ConfigService<ConfigType, true>)
+        city = configService.get('CITY')
         authController = app.get<AuthController>(AuthController)
     })
 
     describe('root', () => {
         it('should return registration User"', () => {
-            expect(authController.getHello()).toBe('Hello World, Moscow')
+            expect(authController.getHello()).toBe(`Hello World, ${city}`)
         })
     })
 })

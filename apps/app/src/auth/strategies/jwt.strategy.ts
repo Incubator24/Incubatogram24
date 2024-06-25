0024cb5395059638dd3cc08f6fb83c0a2966b9c3
Rosh95 <azi.rovshan@gmail.com>
2024-06-25 18:33:34 +0300

    hot fixes 2

--- apps/app/src/auth/strategies/jwt.strategy.ts
+++ apps/app/src/auth/strategies/jwt.strategy.ts
@@ -10,6 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         private readonly configService: ConfigService<ConfigType, true>
     ) {
         const secretOrKey = configService.get<string>('JWT_SECRET')
+
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,

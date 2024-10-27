e8eaf00d4acf0648c4052f7d0d3d0a2fe4f4618b
Alexey Kapralov <alewka24@gmail.com>
2024-10-27 18:50:01 +0300

    fix

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -94,6 +94,7 @@ const useCases = [ChangeUserConfirmationCode, ValidatePasswordRecoveryCode]
         UsersService,
         GithubService,
         ...repositories,
+        //
         ...useCases,
         {
             provide: 'AUTH_SERVICE',

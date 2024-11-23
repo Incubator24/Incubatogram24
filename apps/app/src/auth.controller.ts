4c8973a380ff00229f8b79d58c3a11c0b003e6b7
Incubator24 <incubator2404@gmail.com>
2024-11-23 18:25:53 +0300

    fix

--- apps/app/src/auth.controller.ts
+++ apps/app/src/auth.controller.ts
@@ -32,7 +32,6 @@ import {
 } from '../../../libs/helpers/types/helpersType'
 import { LocalAuthGuard } from '../../../libs/guards/local-auth.guard'
 import { LoginEndpoint } from '../../../libs/swagger/auth/login'
-import { UserId } from '../../auth/src/api/decorators/user.decorator'
 import { ResponseAccessTokenViewDTO } from '../../auth/src/api/dto/ResponseAccessTokenViewDTO'
 import { RefreshTokenEndpoint } from '../../../libs/swagger/auth/refreshTokenEndpoint'
 import { LogoutEndpoint } from '../../../libs/swagger/auth/logoutEndpoint'
@@ -49,12 +48,13 @@ import Configuration from '../../../libs/config/configuration'
 import { GoogleEndpoint } from '../../../libs/swagger/auth/googleEndpoint'
 import { ValidatePasswordRecoveryCodeCommand } from '../../auth/src/application/use-cases/ValidPasswordRecoveryCode'
 import { ClientProxy } from '@nestjs/microservices'
-import { Cookies } from '../../auth/src/api/decorators/auth.decorator'
 import { SwaggerGetRegistrationConfirmationEndpoint } from '../../../libs/swagger/Internal/swaggerGetNewPasswordEndpoint'
 import { SwaggerPostRegistrationConfirmationEndpoint } from '../../../libs/swagger/Internal/swaggerPostNewPasswordEndpoint'
 import { EmailResendingDto } from '../../auth/src/api/dto/EmailResendingDto'
 import { ApiExcludeEndpoint } from '@nestjs/swagger'
 import { Cookies } from '../../../libs/decorators/auth.decorator'
+import { UserId } from '../../../libs/decorators/user.decorator'
+import { SwaggerPostGoogleEndpoint } from '../../../libs/swagger/Internal/swaggerPostGoogleEndpoint'
 
 @Injectable()
 @Controller('auth')
